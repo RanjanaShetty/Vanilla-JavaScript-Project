db619aaa232fb6232a8be99f9792fc673ead641d
RanjanaShetty <92917118+RanjanaShetty@users.noreply.github.com>
2024-04-13 17:00:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const colors = ["#FF5733", "#FFC300", "#DAF7A6", "#9BF6FF", "#D5D8DC", "#CD5C5C", "#76D7C4", "#FF7F50", "#6495ED", "#ADFF2F", "#FFD700", "#00FF00"];
-
-const btn = document.getElementById('btn');
-const colorText = document.getElementById('color');
-
-btn.addEventListener('click', () => {
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colors[randomNumber];
-  colorText.textContent = colors[randomNumber];
-});
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
-}
+function toggleAccordion(id) {
+    const accordionItem = document.getElementById(id);
+    const accordionContent = accordionItem.querySelector(".accordion-content");
+    const accordionBtn = accordionItem.querySelector(".accordion-btn");
+  
+    if (accordionContent.style.display === "block") {
+      accordionContent.style.display = "none";
+      accordionBtn.textContent = "+";
+    } else {
+      accordionContent.style.display = "block";
+      accordionBtn.textContent = "-";
+    }
+  }
+  
